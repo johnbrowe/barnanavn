f91a1604b7ccbd3c77c9642a83a1288b7c93dcec
John Høj Andreassen <john.andreassen83@gmail.com>
2017-10-15 17:42:15 +0200

    Add increment and restart actions

--- src/actions/name-actions.js
+++ src/actions/name-actions.js
@@ -1,5 +1,8 @@
 export const ADD_ACCEPT_NAME = 'ADD_ACCEPT_NAME';
 export const ADD_REJECT_NAME = 'ADD_REJECT_NAME';
+export const INCREMENT = 'INCREMENT';
+export const RESTART = 'RESTART';
+export const GET_NAMES = 'GET_NAMES';
 
 export function addAcceptName(name) {
   return {
@@ -9,8 +12,26 @@ export function addAcceptName(name) {
 }
 
 export function addRejectName(name) {
-    return {
-      type: ADD_REJECT_NAME,
-      payload: name
-    }
+  return {
+    type: ADD_REJECT_NAME,
+    payload: name
   }
+}
+
+export function increment() {
+  return {
+    type: INCREMENT
+  }
+}
+
+export function restart() {
+  return {
+    type: RESTART
+  }
+}
+
+export function getNames() {
+  return {
+    type: GET_NAMES,
+  }
+}
