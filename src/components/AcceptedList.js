d1cbb705c947263e0a08994ba4a693a6fde640b2
John Høj Andreassen <john.andreassen83@gmail.com>
2017-10-16 22:50:20 +0200

    Create AcceptedList

---
+++ src/components/AcceptedList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './../App.css';
+import { CSSTransitionGroup } from 'react-transition-group'
+import { connect } from 'react-redux';
+import store from './../store.js';
+
+
+class List extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Bindings
+    this.noneSelectedYetMsg = this.noneSelectedYetMsg.bind(this);
+
+  }
+
+  noneSelectedYetMsg() {
+    if (this.props.accepted.length == 0) {
+      return "Onki navn"
+    } else {
+      return "";
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><b>Nøvn:</b></li>
+          {this.noneSelectedYetMsg()}
+
+          {this.props.accepted.map((data, i) => {
+            return <li key={i}>
+              {data.name}
+            </li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = function (store) {
+  return {
+    accepted: store.names.accepted
+  };
+}
+
+export default connect(mapStateToProps)(List);
