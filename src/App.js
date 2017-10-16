feee96aa66f24179fc69201a32d6529bb23d0b26
John Høj Andreassen <john.andreassen83@gmail.com>
2017-10-16 22:50:57 +0200

    Add Switch and menu

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import { connect } from 'react-redux';
 import SelectGender from './components/SelectGender.js';
 import store from './store.js';
 import Restart from './components/Restart.js';
-import Name from './components/Name.js';
-import Description from './components/Description.js';
-import Progress from './components/Progress.js';
-import Buttons from './components/Buttons.js';
-import List from './components/List.js';
+import Action from './components/Action.js';
+import AcceptedList from './components/AcceptedList.js';
+import RejectedList from './components/RejectedList.js';
+import { Switch, Route } from 'react-router-dom'
 
 
 class App extends Component {
@@ -23,21 +22,18 @@ class App extends Component {
       display = <SelectGender></SelectGender>
     } else {
       display = <div>
-        <Restart></Restart>
-        <section>
-          <div className="section">
-            <Name></Name>
-            <Description></Description>
-            <br />
-            <Progress></Progress>
-            <br />
-            <Buttons>
-            </Buttons>
-            <br />
-            <br />
-            <List></List>
-          </div>
-        </section>
+        <Restart></Restart> 
+        <ul className="tab-menu">
+          <li><a href="./nei">Nei</a></li>
+          <li><a href="./navn">Navn</a></li>
+          <li><a href="./ja">Ja</a></li>
+        </ul>
+        <Switch>
+          <Route exact path='/navn' component={Action}/>
+          <Route path='/nei' component={RejectedList}/>
+          <Route path='/ja' component={AcceptedList}/>
+        </Switch>
+        
       </div>;
     }
 
