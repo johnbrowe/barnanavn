e9f3b3424a64cac17651d8d4a365b75f3527703c
John Høj Andreassen <john.andreassen83@gmail.com>
2017-10-16 22:49:57 +0200

    Add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -26,8 +27,10 @@ window.react = React;
 window.store = store;
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
     document.getElementById('root'));
 registerServiceWorker();
