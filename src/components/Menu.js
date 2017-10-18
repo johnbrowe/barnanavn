e9348999dba50b9685fa3226f86da941be33f032
John Høj Andreassen <john.andreassen83@gmail.com>
2017-10-18 21:49:09 +0200

    Add menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => (
+    <ul className="tab-menu">
+        <li><a href="./nei">Nei</a></li>
+        <li><a href="/navn">Navn</a></li>
+        <li><a href="./ja">Ja</a></li>
+    </ul>
+);
+
+export default Menu;
