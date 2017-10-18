7b78ffb30ad225839e5d91f97bbb73414bb4658b
John Høj Andreassen <john.andreassen83@gmail.com>
2017-10-18 21:47:45 +0200

    Move menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import SelectGender from './components/SelectGender.js';
 import store from './store.js';
 import Restart from './components/Restart.js';
+import Progress from './components/Progress.js';
 import Action from './components/Action.js';
 import AcceptedList from './components/AcceptedList.js';
 import RejectedList from './components/RejectedList.js';
@@ -19,28 +20,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-        <Restart></Restart> 
-        <ul className="tab-menu">
-          <li><a href="./nei">Nei</a></li>
-          <li><a href="/">Navn</a></li>
-          <li><a href="./ja">Ja</a></li>
-        </ul>
+        <Restart></Restart>
         <div className="action-container">
-          <Switch>
-            <Route exact path='/' component={SelectGender}/>
-            <Route exact path='/navn' component={Action}/>
-            <Route exact path='/nei' component={RejectedList}/>
-            <Route exact path='/ja' component={AcceptedList}/>
-          </Switch>
+          <div className="action-box">
+            <Switch>
+              <Route exact path='/' component={SelectGender} />
+              <Route exact path='/navn' component={Action} />
+              <Route exact path='/nei' component={RejectedList} />
+              <Route exact path='/ja' component={AcceptedList} />
+            </Switch>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = function(store) {
+const mapStateToProps = function (store) {
   return {
     gender: store.gender
   };
