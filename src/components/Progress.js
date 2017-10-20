642d7b6a1b4170b5a9ec522f3511386240685c02
John Høj Andreassen <john.andreassen83@gmail.com>
2017-10-20 17:29:18 +0200

    Remove unused component

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import store from '../store.js';
-import { Line, Circle } from 'rc-progress';
+import { Line } from 'rc-progress';
 
 
 class Progress extends Component {
