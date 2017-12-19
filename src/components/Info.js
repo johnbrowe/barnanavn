06a7104bec6568af00e883296c7f5642936bb892
John Høj Andreassen <john.andreassen83@gmail.com>
2017-12-19 14:25:38 +0100

    Improve info page

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,56 +1,35 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import store from '../store.js';
-import { restart } from '../actions/name-actions';
-import { selectGender } from '../actions/gender-actions';
-import MdBack from 'react-icons/lib/md/keyboard-arrow-left';
-import MdClose from 'react-icons/lib/md/close';
+import React from 'react';
 
-import { withRouter } from 'react-router';
+const Info = () => (
+    <div className="page">
+        <h2>Søgan um Navnabretti</h2>
+        <p>
+            Hugskotid er at gera tad lættari og stuttuligari hjá tær at finna eitt navn. Tad er eitt sindur óinteresant
+            at ganga í 900 nøvn frá á-z. Eg hevdi ein mistanka at nøvnini komu ikki heilt til sín rætt í alfabetiskum ordan og helt hetta kundi gerast meiri visuelt spennandi.
+            Vid hesum fyri eyga er appin bygd. Okkara barn fekk navnid <b>Jónatan</b>, vónandi finnur tú eisini eitt til títt barn.
+            <br/>
+            <br/>
+            <b>Bestu ynskir</b><br/>
+            <b>John Høj Andreassen</b>
+        </p>
+        <br/>
+        <h2>
+            FAQ
+        </h2>
+        <ul>
+            <li>
+                <b>Kann nakar síggja nøvnini sum eg havi valt?</b>
+                <br/>
+                <b>Svar:</b> Nøvnini eru goymd á tínum browsara, inntil tú trýstur á umaftur.
+                Tá vera øll nøvnini slettadi, og tú kanst byrja av nýggjum.
+            </li>
+            <li>
+                <b>Hvar eru nøvnini frá?</b>
+                <br/>
+                <b>Svar:</b> Nøvnini eru frá <a href="www.malrad.fo">malrad.fo</a>
+            </li>
+        </ul>
+    </div>
+);
 
-class Restart extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="info">
-                <div className="info-window">
-                    <h2>Søgan um barnanavn</h2>
-                    <p>
-                        Appin var gjørd vid tí endamálid at finna eitt navn til mítt nýfødda barn. Eg helt tad var eitt sindur óinteresant
-                        at ganga í gjøgnum nøvnini frá á-z. Eg var sikkur uppá at hetta kundi gerast meiri visuelt spennandi
-                        og eg hevdi ein mistanka um at nøvnini komu ikki heilt til sín rætt í alfabetiskum ordan. Útfrá hesum útkom
-                        barnanavn appin. Okkara barn fekk navnid <b>Jónatan</b>, vónandi finnur tú eisini eitt til títt barn.
-                        <br />
-                        <br />
-                        Bestu ynskir<br />
-                        <b>John Høj Andreassen</b>
-                    </p>
-                    <br />
-                    <h2>
-                        FAQ
-                    </h2>
-                    <ul>
-                        <li>
-                            <b>Kann nakar síggja nøvnini sum eg havi valt?</b>
-                            <br />
-                            <b>Svar:</b> Nøvnini eru goymd á tínum browsara, inntil tú trýstur á umaftur.
-                            Tá vera øll nøvnini slettadi, og tú kanst byrja av nýggjum.
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
-}
-
-const RestartWithRouter = withRouter(Restart);
-const mapStateToProps = function (store) {
-    return {
-        info: store.info
-    };
-};
-export default connect(mapStateToProps)(RestartWithRouter);
+export default Info;
