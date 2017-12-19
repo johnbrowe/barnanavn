44acddc6e1067f76d98be0687b631c325282fbb8
John Høj Andreassen <john.andreassen83@gmail.com>
2017-12-19 13:39:22 +0100

    Clean up App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import SelectGender from './components/SelectGender.js';
-import store from './store.js';
 import Info from './components/Info.js';
-import Progress from './components/Progress.js';
 import Action from './components/Action.js';
 import AcceptedList from './components/AcceptedList.js';
 import RejectedList from './components/RejectedList.js';
@@ -15,34 +13,34 @@ import GlobalMenu from './components/GlobalMenu';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <GlobalMenu />
-        <div className="action-container">
-          <div className="action-box">
-            <Switch>
-              <Route exact path='/' component={SelectGender} />
-              <Route exact path='/navn' component={Action} />
-              <Route exact path='/nei' component={RejectedList} />
-              <Route exact path='/ja' component={AcceptedList} />
-              <Route exact path='/info' component={Info} />
-            </Switch>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <GlobalMenu/>
+                <div className="action-container">
+                    <div className="action-box">
+                        <Switch>
+                            <Route exact path='/' component={SelectGender}/>
+                            <Route exact path='/navn' component={Action}/>
+                            <Route exact path='/nei' component={RejectedList}/>
+                            <Route exact path='/ja' component={AcceptedList}/>
+                            <Route exact path='/info' component={Info}/>
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-const mapStateToProps = function (store) {
-  return {
-    gender: store.gender
-  };
+const mapStateToProps = function(store) {
+    return {
+        gender: store.gender
+    };
 };
 
 
