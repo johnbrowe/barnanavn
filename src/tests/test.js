85928e801b34315e9087b638e4dab41dca824088
John Høj Andreassen <john.andreassen83@gmail.com>
2018-02-27 16:23:31 +0100

    Prove that user is not redirected when hitting /navn without data

--- src/tests/test.js
+++ src/tests/test.js
@@ -36,3 +36,19 @@ test('I should see a boy name', async () => {
 
     browser.close();
 }, 16000);
+
+test('I be redirected to homescreen', async () => {
+    let browser = await puppeteer.launch({
+        headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto('http://localhost:3000/navn');
+    await page.waitForSelector('.App');
+
+    const html = await page.$eval('.action-container', e => e.innerHTML);
+    expect(html).toContain('Drongur');
+    expect(html).toContain('Genta');
+    browser.close();
+}, 16000);
+    
