88cfd7b51df635469576330bbb1b957333022888
John Høj Andreassen <john.andreassen83@gmail.com>
2018-06-04 17:16:58 +0200

    Animate name and description

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,41 +1,58 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import posed from 'react-pose';
+
+const Box = posed.div({
+  visible: { opacity: 1, transition: { duration: 1000 } },
+  hidden: { opacity: 0, transition: { duration: 0 } }
+});
+
 
 class Description extends Component {
-    isFinished() {
-        return (this.props.names.index + 1) > this.props.names[this.props.gender.selected].length
-    }
+  state = { isVisible: false };
 
-    showDesc() {
-        if(!this.isFinished()) {
-            return this.props.names[this.props.gender.selected][this.props.names.index].desc
-        }
-    }
+  componentWillReceiveProps() {
+    this.setState({ isVisible: false });
 
-    showID() {
-        if(!this.isFinished()) {
-            return this.props.names[this.props.gender.selected][this.props.names.index].id
-        } else {
-            return "Onki navn eftir"
-        }
-    }
+    setTimeout(() => {
+      this.setState({ isVisible: true });
+    }, 1);
+  }
 
-    render() {
-        return (
-            <div>
-              <h2 className="name-description" key={this.showID()}>
-                  <small><i>{this.showDesc()}</i></small>
-              </h2>
-            </div>
-        );
+  isFinished() {
+    return (this.props.names.index + 1) > this.props.names[this.props.gender.selected].length
+  }
+
+  showDesc() {
+    if (!this.isFinished()) {
+      return this.props.names[this.props.gender.selected][this.props.names.index].desc
     }
+  }
+
+  showID() {
+    if (!this.isFinished()) {
+      return this.props.names[this.props.gender.selected][this.props.names.index].id
+    } else {
+      return "Onki navn eftir"
+    }
+  }
+
+  render() {
+    return (
+      <Box className="box" pose={this.state.isVisible ? 'visible' : 'hidden'} >
+        <h2 className="name-description" key={this.showID()}>
+          <small><i>{this.showDesc()}</i></small>
+        </h2>
+      </Box>
+    );
+  }
 };
 
-const mapStateToProps = function(store) {
-    return {
-        names : store.names,
-        gender: store.gender
-    };
+const mapStateToProps = function (store) {
+  return {
+    names: store.names,
+    gender: store.gender
+  };
 };
 
 export default connect(mapStateToProps)(Description);
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,43 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import posed from 'react-pose';
+
+const Box = posed.div({
+  visible: { opacity: 1, transition: { duration: 500 } },
+  hidden: { opacity: 0, transition: { duration: 0 }}
+});
 
 class Name extends Component {
-    isFinished() {
-        return (this.props.names[this.props.gender.selected].index + 1) > this.props.names[this.props.gender.selected].length
-    }
 
-    showName() {
-        if (!this.isFinished()) {
-            return this.props.names[this.props.gender.selected][this.props.names.index].name
-        } else {
-            return "Onki navn eftir"
-        }
-    }
+  state = { isVisible: false };
 
-    showID() {
-        if (!this.isFinished()) {
-            return this.props.names[this.props.gender.selected][this.props.names.index].id
-        } else {
-            return "Onki navn eftir"
-        }
-    }
+  componentWillReceiveProps() {
+    this.setState({ isVisible: false });
+    
+    setTimeout(() => {
+      this.setState({ isVisible: true });
+    }, 1);
+  }
 
-    render() {
-        return (
-            <div>
-              <h1 className="display-name" key={this.showID()}>{this.showName()}</h1>
-            </div>
-        );
+  isFinished() {
+    return (this.props.names[this.props.gender.selected].index + 1) > this.props.names[this.props.gender.selected].length
+  }
+
+  showName() {
+    if (!this.isFinished()) {
+      return this.props.names[this.props.gender.selected][this.props.names.index].name
+    } else {
+      return "Onki navn eftir"
     }
+  }
+
+  showID() {
+    if (!this.isFinished()) {
+      return this.props.names[this.props.gender.selected][this.props.names.index].id
+    } else {
+      return "Onki navn eftir"
+    }
+  }
+
+  render() {
+    return (
+      <Box className="box" pose={this.state.isVisible ? 'visible' : 'hidden'} >
+        <h1 className="display-name" key={this.showID()}>{this.showName()}</h1>
+      </Box>
+    );
+  }
 }
 
 
 const mapStateToProps = function (store) {
 
-    return {
-        names: store.names,
-        gender: store.gender
-    };
+  return {
+    names: store.names,
+    gender: store.gender
+  };
 }
 
 export default connect(mapStateToProps)(Name);
