67c8c16a6057bc2b473cc08059017570712006d4
John Høj Andreassen <john@techcare.of>
2019-02-18 22:49:52 +0000

    Add more feedback + rollbar only in production

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,77 +1,97 @@
-import React, { Component } from 'react';
-import MdCheck from 'react-icons/lib/md/check';
-import MdClear from 'react-icons/lib/md/clear';
-import { addAcceptName, addRejectName, increment } from '../actions/name-actions';
-import { connect } from 'react-redux';
-import store from '../store.js';
+import React, { Component } from "react";
+import MdCheck from "react-icons/lib/md/check";
+import MdClear from "react-icons/lib/md/clear";
+import {
+  addAcceptName,
+  addRejectName,
+  increment
+} from "../actions/name-actions";
+import { connect } from "react-redux";
+import store from "../store.js";
 
 class Buttons extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      buttonDisable: false
+    };
 
-        this.state = {
-            buttonDisable: false
-        };
+    // Bindings
+    this.rejectName = this.rejectName.bind(this);
+    this.acceptName = this.acceptName.bind(this);
+    this.disableButton = this.disableButton.bind(this);
+  }
 
-        // Bindings
-        this.rejectName = this.rejectName.bind(this);
-        this.acceptName = this.acceptName.bind(this);
-        this.disableButton = this.disableButton.bind(this);
+  isFinished() {
+    let count = 0;
+    if (this.props.gender.selected == "male") {
+      count = this.props.names.maleCount;
+    } else {
+      count = this.props.names.femaleCount;
     }
 
-    isFinished() {
-      let count = 0;
-      if(this.props.gender.selected == 'male'){
-        count = this.props.names.maleCount;
-      } else {
-        count = this.props.names.femaleCount;
-      }
+    return (
+      this.props.names.accepted.length + this.props.names.rejected.length >=
+      count
+    );
+  }
 
-      return (this.props.names.accepted.length + this.props.names.rejected.length) >= count;  
-    }
+  rejectName() {
+    this.disableButton();
+    store.dispatch(
+      addRejectName(
+        this.props.names[this.props.gender.selected][this.props.names.index]
+      )
+    );
+    store.dispatch(increment());
+  }
 
-    rejectName() {
-        this.disableButton();
-        store.dispatch(addRejectName(this.props.names[this.props.gender.selected][this.props.names.index]));
-        store.dispatch(increment());
-    }
+  acceptName() {
+    this.disableButton();
+    store.dispatch(
+      addAcceptName(
+        this.props.names[this.props.gender.selected][this.props.names.index]
+      )
+    );
+    store.dispatch(increment());
+  }
 
-    acceptName() {
-        this.disableButton();
-        store.dispatch(addAcceptName(this.props.names[this.props.gender.selected][this.props.names.index]));
-        store.dispatch(increment());
-    }
+  disableButton() {
+    // Disable button
+    this.setState({ buttonDisable: true });
+    // Endable button again after x amount of seconds
+    setTimeout(() => this.setState({ buttonDisable: false }), 200);
+  }
 
-    disableButton(){
-        // Disable button
-        this.setState({buttonDisable:true});
-        // Endable button again after x amount of seconds
-        setTimeout(() => this.setState({buttonDisable:false}), 1000);
-    }
-
-    render() {
-        return (
-            <div className="buttons">
-                <button className="button button-no" disabled={this.isFinished() || this.state.buttonDisable} onClick={this.rejectName}>
-                    <MdClear />
-                </button>
-                &nbsp;&nbsp;&nbsp;
-                <button className="button button-yes" disabled={this.isFinished() || this.state.buttonDisable} onClick={this.acceptName}>
-                    <MdCheck />
-                </button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="buttons">
+        <button
+          className="button button-no"
+          disabled={this.isFinished() || this.state.buttonDisable}
+          onClick={this.rejectName}
+        >
+          <MdClear />
+        </button>
+        &nbsp;&nbsp;&nbsp;
+        <button
+          className="button button-yes"
+          disabled={this.isFinished() || this.state.buttonDisable}
+          onClick={this.acceptName}
+        >
+          <MdCheck />
+        </button>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = function (store) {
-
-    return {
-        names: store.names,
-        gender: store.gender
-    };
+const mapStateToProps = function(store) {
+  return {
+    names: store.names,
+    gender: store.gender
+  };
 };
 
 export default connect(mapStateToProps)(Buttons);
-   
