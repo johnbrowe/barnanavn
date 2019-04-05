3f6904b1de851328a66f62e096040b96cf676597
John Høj Andreassen <john@techcare.of>
2019-04-05 12:43:50 +0200

    Add list counter

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import {NavLink} from 'react-router-dom'
 
-const Menu = () => (
-    <ul className="tab-menu">
-        <li>
-            <NavLink to="/nei">Nei</NavLink>
-        </li>
-        <li>
-            <NavLink to="/navn">Navn</NavLink>
-        </li>
-        <li>
-            <NavLink to="/ja">Ja</NavLink>
-        </li>
-    </ul>
-)
+interface Props {
+    accepted: string[]
+    rejected: string[]
+}
 
-export default Menu
+class Menu extends Component<Props, any> {
+    render() {
+        return (
+            <ul className="tab-menu">
+                <li style={{position: 'relative'}}>
+                    <NavLink to="/nei">
+                        Nei{' '}
+                        <div className="list-counter list-counter-no">
+                            {this.props.rejected.length}
+                        </div>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/navn">Navn</NavLink>
+                </li>
+                <li style={{position: 'relative'}}>
+                    <NavLink to="/ja">
+                        Ja{' '}
+                        <div className="list-counter list-counter-yes">
+                            {this.props.accepted.length}
+                        </div>
+                    </NavLink>
+                </li>
+            </ul>
+        )
+    }
+}
+
+const mapStateToProps = (store: any) => {
+    return {
+        rejected: store.names.rejected,
+        accepted: store.names.accepted
+    }
+}
+
+export default connect(mapStateToProps)(Menu)
