f54beaa755f4f37ed3bd48eb3d8e18a9cef54917
John Høj Andreassen <john@techcare.of>
2019-04-05 12:04:33 +0200

    Remove rc-progress component

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,31 +1,20 @@
-import {Line} from 'rc-progress'
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 class Progress extends Component<any, any> {
-    progress() {
+    render() {
         const total = this.props.names[this.props.gender.selected + 'Count']
         const actual = this.props.names.accepted.length + this.props.names.rejected.length + 1
-        return (actual * 100) / total
-    }
 
-    render() {
         return (
-            <div className="progress-box">
-                <Line
-                    percent={this.progress()}
-                    strokeWidth="1.5"
-                    trailWidth="1.5"
-                    strokeColor="#118C8B"
-                    trailColor="#F1f1f1"
-                    strokeLinecap="square"
-                />
-            </div>
+            <p className="progress">
+                {actual}/{total}
+            </p>
         )
     }
 }
 
-const mapStateToProps = (store: any ) => {
+const mapStateToProps = (store: any) => {
     return {
         names: store.names,
         gender: store.gender
